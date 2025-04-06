8f723954b7774fb52ff56f3bcd9d55d23c5e91d6
Ed <ed@fossx.ch>
2025-04-07 00:31:12 +0200

    format

--- src/cwutils/datasets/_base.py
+++ src/cwutils/datasets/_base.py
@@ -5,6 +5,7 @@ from importlib import resources
 DATA_MODULE = "cwutils.datasets.data"
 DESCR_MODULE = "cwutils.datasets.descr"
 
+
 def load_csv_data(
     data_file_name,
     /,
@@ -13,7 +14,7 @@ def load_csv_data(
     data_module=DATA_MODULE,
     descr_file_name=None,
     descr_module=DESCR_MODULE,
-    separate_target = False,
+    separate_target=False,
     encoding="utf-8",
     **kwargs,
 ):
@@ -24,7 +25,7 @@ def load_csv_data(
     data_file_name : str
         Name of csv file to be loaded from `data_module/data_file_name`.
         For example `advertising.csv`.
-    
+
     target : str or int
         Name or the index of the target column.
 
@@ -34,12 +35,12 @@ def load_csv_data(
     descr_file_name : str, default=None
         Name of rst file to be loaded from `descr_module/descr_file_name`.
         For example `'advertising.rst'`. See also :func:`load_descr`.
-        If not None, also returns the corresponding description of 
+        If not None, also returns the corresponding description of
         the dataset.
-    
+
     separate_target : bool, default=False
-        If true, split the dataset into design matrix of shape (n_samples, n_features) 
-        and target column of shape (n_samples,) and return them separately. 
+        If true, split the dataset into design matrix of shape (n_samples, n_features)
+        and target column of shape (n_samples,) and return them separately.
         Return the entire dataset otherwise.
 
     encoding: str, optional
@@ -48,7 +49,7 @@ def load_csv_data(
     Returns
     -------
     design matrix : :class:`pd.DataFrame` of shape (n_samples, n_features)
-        A 2D data frame with each row representing one sample and each column 
+        A 2D data frame with each row representing one sample and each column
         representing the features of a given sample.
 
     target : :class:`pd.Series` of shape (n_samples,)
@@ -65,7 +66,6 @@ def load_csv_data(
         first_row = csv_file.readline()
         dialect = csv.Sniffer().sniff(first_row)
 
-    
     dialect = None if dialect.delimiter == "," else dialect
 
     df = pd.read_csv(data_path, encoding=encoding, dialect=dialect, **kwargs)
@@ -76,17 +76,24 @@ def load_csv_data(
                 return df.drop(target, axis=1), target_series
             else:
                 assert descr_module is not None
-                descr = load_descr(descr_module=descr_module, descr_file_name=descr_file_name)
+                descr = load_descr(
+                    descr_module=descr_module, descr_file_name=descr_file_name
+                )
                 return df.drop(target, axis=1), target_series, descr
         except KeyError:
-            raise KeyError(f"Specified target name `{target}` is not in the columnd of the dataset.")
+            raise KeyError(
+                f"Specified target name `{target}` is not in the columnd of the dataset."
+            )
     else:
         if descr_file_name is None:
             return df
         else:
             assert separate_target is not None
-            descr = load_descr(descr_module=descr_module, descr_file_name=descr_file_name)
+            descr = load_descr(
+                descr_module=descr_module, descr_file_name=descr_file_name
+            )
             return df, descr
+
 
 def load_descr(descr_file_name, *, descr_module=DESCR_MODULE, encoding="utf-8"):
     """Load `descr_file_name` from `descr_module` with `importlib.resources`.
@@ -115,8 +122,10 @@ def load_descr(descr_file_name, *, descr_module=DESCR_MODULE, encoding="utf-8"):
     path = resources.files(descr_module) / descr_file_name
     return path.read_text(encoding=encoding)
 
+
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
