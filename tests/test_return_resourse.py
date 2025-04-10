4c6d3c90acb0de6d43c726ad2b0c1d03d67bdfbd
Ed <ed@fossx.ch>
2025-04-10 21:08:51 +0200

    rm shared fixtures

--- tests/test_return_resourse.py
+++ tests/test_return_resourse.py
@@ -4,45 +4,20 @@ from cwutils.datasets._base import _return_resource
 import pytest
 
 
-@pytest.fixture
-def example_csv_name() -> str:
-    return "advertising_synth.csv"
-
-
-@pytest.fixture
-def default_data_module() -> str:
-    return "cwutils.datasets.data"
-
-
-@pytest.fixture
-def default_descr_module() -> str:
-    return "cwutils.datasets.descr"
-
-
-@pytest.fixture
-def nonexistent_module_name() -> str:
-    return "thismoduledoesnotexist"
-
-
-@pytest.fixture
-def nonexistent_file_name() -> str:
-    return "thisfiledoesnotexist.csv"
-
-
-def test_return_type(default_data_module, example_csv_name) -> None:
-    path = _return_resource(default_data_module, example_csv_name)
+def test_return_type(default_data_module, correct_csv_name) -> None:
+    path = _return_resource(default_data_module, correct_csv_name)
     assert isinstance(path, os.PathLike)
 
 
-def test_return_value(default_data_module, example_csv_name) -> None:
-    path = _return_resource(default_data_module, example_csv_name)
-    test_value = importlib.resources.files(default_data_module) / example_csv_name
+def test_return_value(default_data_module, correct_csv_name) -> None:
+    path = _return_resource(default_data_module, correct_csv_name)
+    test_value = importlib.resources.files(default_data_module) / correct_csv_name
     assert path == test_value
 
 
-def test_nonexistent_module_input(nonexistent_module_name, example_csv_name) -> None:
+def test_nonexistent_module_input(nonexistent_module_name, correct_csv_name) -> None:
     with pytest.raises(ModuleNotFoundError):
-        _return_resource(nonexistent_module_name, example_csv_name)
+        _return_resource(nonexistent_module_name, correct_csv_name)
 
 
 def test_nonexistent_file_input(default_data_module, nonexistent_file_name) -> None:
@@ -53,3 +28,8 @@ def test_nonexistent_file_input(default_data_module, nonexistent_file_name) -> N
 def test_wrong_is_file(default_data_module, tmpdir) -> None:
     with pytest.raises(AssertionError):
         _return_resource(default_data_module, tmpdir)
+
+
+def test_dir_input() -> None:
+    with pytest.raises(IsADirectoryError):
+        _return_resource("cwutils.datasets", "data")
